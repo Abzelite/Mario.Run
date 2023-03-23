f7cb4cafa17189b8a74a40394e13ed216f091c4b
Abzelite <109804681+Abzelite@users.noreply.github.com>
2023-03-23 10:48:18 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,44 @@
+let noseX = 0;
+let noseY = 0;
+let capture
+
 function preload() {
-	// loadingAllSounds();  
-	setSprites();
-	MarioAnimation();
+  world_start = loadSound("world_start.wav");
+  // Assuming setSprites() and MarioAnimation() are defined elsewhere
+  setSprites();
+  MarioAnimation();
 }
+
+let canvas;//turning on webcam in p5.js?
+
 
 function setup() {
-	createCanvas(gameConfig.screenX,gameConfig.screenY);
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+
 	instializeInSetup(mario);
-	// playBGMusic();
-}
+	
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
 
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+
+}
+function modelLoaded()
+{
+  console.log('mm')
+}
 function draw() {
-	game()
+  game(); // Assuming game() is defined elsewhere?
+
 }
 
-
-
-
-
-
+function gotPoses(results) {
+  if (results.length > 0) {
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log(results);
+  }
+}
